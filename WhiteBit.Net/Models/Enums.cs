448f3b5bff141a442af6bbdb06ce692b35aaad39
alokym86 <alokym86@gmail.com>
2022-10-26 13:39:04 +0300

    socket methods enum

--- WhiteBit.Net/Models/Enums.cs
+++ WhiteBit.Net/Models/Enums.cs
@@ -67,17 +67,143 @@ namespace WhiteBit.Net.Models.Enums
         Failed
     }
 
-    public enum SocketMethod
+    public enum SocketOutgoingMethod
     {
         [EnumMember(Value = "authorize")]
         Authorize,
+
         [EnumMember(Value = "balanceSpot_request")]
         BalanceSpotRequest,
+
         [EnumMember(Value = "balanceSpot_subscribe")]
         BalanceSpotSubscribe,
-        [EnumMember(Value = "balanceSpot_update")]
-        BalanceSpotUpdate,
+
         [EnumMember(Value = "balanceSpot_unsubscribe")]
         BalanceSpotUnsubscribe,
+
+        [EnumMember(Value = "balanceMargin_request")]
+        BalanceMarginRequest,
+
+        [EnumMember(Value = "balanceMargin_subscribe")]
+        BalanceMarginSubscribe,
+
+        [EnumMember(Value = "balanceMargin_unsubscribe")]
+        BalanceMarginUnsubscribe,
+
+        [EnumMember(Value = "ordersPending_request")]
+        ActiveOrdersRequest,
+
+        [EnumMember(Value = "ordersPending_subscribe")]
+        ActiveOrdersSubscribe,
+
+        [EnumMember(Value = "ordersPending_unsubscribe")]
+        ActiveOrdersUnsubscribe,
+
+        [EnumMember(Value = "ordersExecuted_request")]
+        ExecutedOrdersRequest,
+
+        [EnumMember(Value = "ordersExecuted_subscribe")]
+        ExecutedOrdersSubscribe,
+
+        [EnumMember(Value = "ordersExecuted_unsubscribe")]
+        ExecutedOrdersUnsubscribe,
+
+        [EnumMember(Value = "deals_request")]
+        UserTradesRequest,
+
+        [EnumMember(Value = "deals_subscribe")]
+        UserTradesSubscribe,
+
+        [EnumMember(Value = "deals_unsubscribe")]
+        UserTradesUnsubscribe,
+
+        [EnumMember(Value = "candles_request")]
+        CandlesRequest,
+
+        [EnumMember(Value = "candles_subscribe")]
+        CandlesSubscribe,
+
+        [EnumMember(Value = "candles_unsubscribe")]
+        CandlesUnsubscribe,
+
+        [EnumMember(Value = "lastprice_request")]
+        LastpriceRequest,
+
+        [EnumMember(Value = "lastprice_subscribe")]
+        LastpriceSubscribe,
+
+        [EnumMember(Value = "lastprice_unsubscribe")]
+        LastpriceUnsubscribe,
+
+        [EnumMember(Value = "market_request")]
+        MarketStatisticRequest,
+
+        [EnumMember(Value = "market_subscribe")]
+        MarketStatisticSubscribe,
+
+        [EnumMember(Value = "market_unsubscribe")]
+        MarketStatisticUnsubscribe,
+
+        [EnumMember(Value = "marketToday_request")]
+        TickerRequest,
+
+        [EnumMember(Value = "marketToday_subscribe")]
+        TickerSubscribe,
+
+        [EnumMember(Value = "marketToday_unsubscribe")]
+        TickerUnsubscribe,
+
+        [EnumMember(Value = "trades_request")]
+        PublicTradesRequest,
+
+        [EnumMember(Value = "trades_subscribe")]
+        PublicTradesSubscribe,
+
+        [EnumMember(Value = "trades_unsubscribe")]
+        PublicTradesUnsubscribe,
+
+        [EnumMember(Value = "depth_request")]
+        OrderBookRequest,
+
+        [EnumMember(Value = "depth_subscribe")]
+        OrderBookSubscribe,
+
+        [EnumMember(Value = "depth_unsubscribe")]
+        OrderBookUnsubscribe,
+    }
+    public enum SocketIncomeMethod
+    {
+        [EnumMember(Value = "balanceSpot_update")]
+        BalanceSpot,
+
+        [EnumMember(Value = "balanceMargin_update")]
+        BalanceMargin,
+
+        [EnumMember(Value = "ordersPending_update")]
+        ActiveOrders,
+
+        [EnumMember(Value = "ordersExecuted_update")]
+        ExecutedOrders,
+
+        [EnumMember(Value = "deals_update")]
+        UserTrades,
+
+        [EnumMember(Value = "candles_update")]
+        Candles,
+
+        [EnumMember(Value = "lastprice_update")]
+        Lastprice,
+
+        [EnumMember(Value = "market_update")]
+        MarketStatistic,
+
+        [EnumMember(Value = "marketToday_update")]
+        Ticker,
+
+        [EnumMember(Value = "trades_update")]
+        PublicTrades,
+
+        [EnumMember(Value = "depth_update")]
+        OrderBook,
     }
 }
