7e1511fea71d3cd30cdaa0c88805ff787d558611
sergii <sergii.polishchuk@gmail.com>
2023-11-14 12:50:58 +0200

    fix version

--- WhiteBit.Net/Clients/WhiteBitRestClient.cs
+++ WhiteBit.Net/Clients/WhiteBitRestClient.cs
@@ -13,11 +13,14 @@ namespace WhiteBit.Net.Clients
         private const string BaseURL = " https://whitebit.com/api/";
         private const string ExchangeName = "WhiteBit";
 
-        //public WhiteBitRestClient(WhiteBitRestClientOptions options) : this(ExchangeName, options)
-        //{
-        //}
+        public WhiteBitRestClient(WhiteBitRestClientOptions options) : this(options, null, ExchangeName)
+        {
+            Initialize(options);
+        }
+
         public WhiteBitRestClient(WhiteBitRestClientOptions options, ILoggerFactory? loggerFactory, string name = ExchangeName) : base(loggerFactory ,name)
         {
+            Initialize(options);
             ApiClient = AddApiClient(new WhiteBitApiClientV4(name, options, new RestApiClientOptions(BaseURL), _logger, this));
         }
 
